7c7149c88a1497cb2362b55f3394c0455bf6fc67
shekhar <shekhar.75880@gmail.com>
2024-04-23 10:27:00 +0530

    Add project files.

---
+++ ApplicationForStudents/Models/Students.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ApplicationForStudents.Models
+{
+
+    [MetadataType(typeof(StudentMetadata))]
+    public partial class Student
+    {
+
+    }
+
+
+    public class StudentMetadata
+    {
+        [Required]
+        [Display(Name ="STUDENT_NO")]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "STUDENT_NAME")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "STUDENT_DOB")]
+        public Nullable<System.DateTime> DOB { get; set; }
+
+        [Required]
+        [Display(Name = "STUDENT_DOJ")]
+        public Nullable<System.DateTime> DOJ { get; set; }
+
+    }
+
+}
